6ab303acf9469faa339d9b0e87f0df12d3827b11
Peter Walsh <peter.t.walsh@stonybrook.edu>
2022-10-25 09:22:40 -0400

    Misc.

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,44 +1,44 @@
 import * as Y from 'yjs'
 
 class CRDTFormat {
-  public bold?: Boolean = false;
-  public italic?: Boolean = false;
-  public underline?: Boolean = false;
+    public bold?: Boolean = false;
+    public italic?: Boolean = false;
+    public underline?: Boolean = false;
 };
 
 exports.CRDT = class {
-  ydoc: any
-  ytext: any
-  cb: Function
+    ydoc: any
+    ytext: any
+    cb: Function
 
-  constructor(cb: (update: string, isLocal: Boolean) => void) {
-    this.ydoc = new Y.Doc();
-    this.ytext = this.ydoc.getText('quill');
-    this.cb = cb;
+    constructor(cb: (update: string, isLocal: Boolean) => void) {
+        this.ydoc = new Y.Doc();
+        this.ytext = this.ydoc.getText('quill');
+        this.cb = cb;
 
-    ['update', 'insert', 'delete', 'toHTML'].forEach(f => (this as any)[f] = (this as any)[f].bind(this));
-  }
+        ['update', 'insert', 'delete', 'toHTML'].forEach(f => (this as any)[f] = (this as any)[f].bind(this));
+    }
 
-  update(update: string) {
-    // ...
-    this.ytext.toDelta()
-    console.log(update);
+    update(update: string) {
+        // ...
+        this.ytext.toDelta()
+        console.log(update);
 
-  }
+    }
 
-  insert(index: number, content: string, format: CRDTFormat) {
-    console.log(format);
-    this.ytext.insert(index, content, { bold: true });
+    insert(index: number, content: string, format: CRDTFormat) {
+        console.log(format);
+        this.ytext.insert(index, content, { bold: true });
 
-  }
+    }
 
-  delete(index: number, length: number) {
-    this.ytext.delete(index, length);
+    delete(index: number, length: number) {
+        this.ytext.delete(index, length);
 
-  }
+    }
 
-  toHTML() {
-    let html = this.ytext.toString();
-    return html;
-  }
+    toHTML() {
+        let html = this.ytext.toString();
+        return html;
+    }
 };
