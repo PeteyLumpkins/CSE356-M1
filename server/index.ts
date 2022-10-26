7f37382b1f32b6558ae7f557aabd59deed1f149a
Jun Ho Lee <junholee366@gmail.com>
2022-10-26 17:51:31 -0400

    Quill GUI added

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,9 @@ import path from 'path';
 import { DocumentSet, Doc, Client } from "./DocumentSet";
 
 const app = express();
+var cors = require('cors')
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use("/", express.static(path.join(__dirname, "public")));
