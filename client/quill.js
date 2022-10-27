cc5affd0d9e243f3a61777ce2912b0fc16905bef
Jun Ho Lee <junholee366@gmail.com>
2022-10-27 11:37:48 -0400

    POST works but is buggy, need to debug both ends of the system

--- client/quill.js
+++ client/quill.js
@@ -36,26 +36,38 @@ window.addEventListener('load', () => {
   // ---------------------------------------- ADDED CODE
   // This code is incomplete...
   // probably need to figure out the yjs logic
-  const docID = "";
+  let eventMutex = false;
+  let docID = "";
   ytext.observe(event => {
-    console.log("yjs.id: " + docID);
+
+    console.log("id: " + docID);
     console.log('delta:', event.changes.delta);
-    // if (docID != "") {
-    //   console.log("[OP] WRITING")
-    //   fetch("http://localhost:3000/api/op/" + docID, { 
-    //     method: "POST",
-    //     headers: { 'Content-Type': 'application/json' },
-    //     body: JSON.stringify({
-    //       id: docID,
-    //       data: event.changes.delta
-    //     })
-    //   })
-    //   console.log('POST delta');
-    // } 
+    
+    if (docID != "" && eventMutex != true) {
+      console.log("[OP] WRITING")
+      fetch("http://localhost:3000/api/op/" + docID, { 
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          id: docID,
+          data: event.changes.delta
+        })
+      })
+      console.log('POST delta');
+    } 
   })
 
+  const setDocID = (id) => {
+    docID = id;
+  }
+
+  const setMutex = (state) => {
+    eventMutex = state;
+  }
+
   // @ts-ignore
-  globalThis.yjs = { ydoc, ytext, binding, Y, docID }
+  globalThis.yjs = { ydoc, ytext, binding, Y }
+  globalThis.doc = { setDocID, setMutex }
   // The keyword "globalThis" allows variables to be "global"
   // JS in quill.html calls these variables by: yjs.<variable>
 })
