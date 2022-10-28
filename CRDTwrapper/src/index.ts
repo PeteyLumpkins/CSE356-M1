e9023ed9e5181042b716fd9096c9d528ce4bbff3
Jun Ho Lee <junholee366@gmail.com>
2022-10-28 12:47:12 -0400

    Included CRDTwrapper

---
+++ CRDTwrapper/src/index.ts
@@ -0,0 +1,55 @@
+import * as Y from 'yjs';
+
+class CRDTFormat {
+  public bold?: Boolean = false;
+  public italic?: Boolean = false;
+  public underline?: Boolean = false;
+};
+
+exports.CRDT = class {
+  ydoc: Y.Doc;
+  ytext: Y.Text; 
+  cb: any;
+
+  constructor(cb: (update: string, isLocal: Boolean) => void) {
+    this.ydoc = new Y.Doc();
+    this.ytext = this.ydoc.getText('quill');
+    this.cb = cb;
+    ['update', 'insert', 'delete', 'toHTML'].forEach(f => (this as any)[f] = (this as any)[f].bind(this));
+  }
+
+  update(update: string) {
+    const event = JSON.parse(update);
+    if (event.event === "sync"){
+      this.ytext.delete(0, this.ytext.length);  // clear yjs text
+    }
+    this.ytext.applyDelta(event.data); 
+  }
+
+  insert(index: number, content: string, format: CRDTFormat) {
+    this.ytext.insert(index, content, format);
+  }
+
+  delete(index: number, length: number) {
+    this.ytext.delete(index, length);
+  }
+
+  toHTML() {
+    let html = '';
+    for (let op of this.ytext.toDelta()) {
+      let text = op.insert;
+      let attr = op.attributes;
+      if (attr ? attr.bold : false) {
+          text = "<b>" + text + "</b>";
+      }
+      if (attr ? attr.italic : false) {
+        text = "<i>" + text + "</i>";
+      }
+      if (attr ? attr.underline : false) {
+        text = "<u>" + text + "</u>";
+      }
+      html += text;
+  }
+    return html;
+  }
+};
