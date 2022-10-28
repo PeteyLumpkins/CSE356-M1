479f5782f1893f3b050364cd39c4331ad611e1c0
Peter Walsh <peter.t.walsh@stonybrook.edu>
2022-10-28 13:21:24 -0400

    Test

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,4 +1,5 @@
 import * as Y from 'yjs'
+// import Quill from 'quill'
 
 class CRDTFormat {
     public bold?: Boolean = false;
@@ -7,38 +8,69 @@ class CRDTFormat {
 };
 
 exports.CRDT = class {
-    ydoc: any
-    ytext: any
+    ydoc: Y.Doc
+    ytext: Y.Text
     cb: Function
+    // editor: Quill;
 
     constructor(cb: (update: string, isLocal: Boolean) => void) {
         this.ydoc = new Y.Doc();
         this.ytext = this.ydoc.getText('quill');
         this.cb = cb;
 
+        // let editorContainer = document.createElement('div')
+        // editorContainer.setAttribute('id', 'editor')
+        // document.body.insertBefore(editorContainer, null)
+
+        // this.editor = new Quill(editorContainer, {
+        //     modules: {
+        //     cursors: true,
+        //     toolbar: [
+        //         [{ header: [1, 2, false] }],
+        //         ['bold', 'italic', 'underline'],
+        //         ['image', 'code-block']
+        //     ],
+        //     history: {
+        //         userOnly: true
+        //     }
+        //     },
+        //     placeholder: 'Start collaborating...',
+        //     theme: 'snow' // or 'bubble'
+        // });
+        
+
         ['update', 'insert', 'delete', 'toHTML'].forEach(f => (this as any)[f] = (this as any)[f].bind(this));
     }
 
     update(update: string) {
-        // ...
-        this.ytext.toDelta()
-        console.log(update);
-
+        this.ytext = new Y.Text(update);
+        console.log(this.ytext.toString());
+        this.cb(this.ytext.toString(), false);
     }
 
     insert(index: number, content: string, format: CRDTFormat) {
-        console.log(format);
-        this.ytext.insert(index, content, { bold: true });
-
+        this.ytext.insert(index, content, format ? { bold: format.bold, italic: format.italic, underline: format.underline } : format);
+        console.log(this.ytext.toString());
+        this.cb(this.ytext.toString(), true);
     }
 
     delete(index: number, length: number) {
         this.ytext.delete(index, length);
-
+        console.log(this.ytext.toString());
+        this.cb(this.ytext.toString(), true);
     }
 
     toHTML() {
-        let html = this.ytext.toString();
+        let html = "";
+        console.log(this.ytext.toDelta());
+        for (let op of this.ytext.toDelta()) {
+            let text = op.insert;
+            let attr = op.attributes;
+            if (attr ? attr.bold : false) {
+                text = "<b>" + text + "</b>";
+            }
+            html += text;
+        }
         return html;
     }
 };
