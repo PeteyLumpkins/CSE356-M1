e22f3bf8e718d115f85404ad56964b88fc78e096
Peter Walsh <peter.t.walsh@stonybrook.edu>
2022-10-29 13:47:14 -0400

    Added crdt wrapper.

--- CRDTwrapper/webpack.config.js
+++ CRDTwrapper/webpack.config.js
@@ -4,47 +4,41 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const JavaScriptObfuscator = require('webpack-obfuscator');
 
 module.exports = {
-  entry: {
-    index: './src/index.ts',
-    quill: './src/quill.js'
-  },
-  output: {
-    filename: 'crdt.js',
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js'
-  },
-  mode: 'production',
-  resolve: {
-    extensions: ['.ts', '.js', '.json'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)?$/,
-        use: {
-          loader: 'babel-loader',
-        },
-        exclude: /node_modules/,
-      },    
-    ]
-  },
-  plugins: [
-    new ForkTsCheckerWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: 'public/index.html'
-    }),
-    new JavaScriptObfuscator({
-      rotateStringArray: true
-    })
-  ],
-  performance: {
-    hints: false,
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000
-  },
-  devServer: {
-    contentBase: path.join(__dirname),
-    compress: true,
-    publicPath: '/dist/'
-  }
+    entry: './src/index.ts',
+    output: {
+        filename: 'crdt.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    mode: 'production',
+    resolve: {
+        extensions: ['.ts', '.js', '.json'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)?$/,
+                use: {
+                    loader: 'babel-loader',
+                },
+                exclude: /node_modules/,
+            },
+        ]
+    },
+    plugins: [
+        new ForkTsCheckerWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'public/index.html'
+        }),
+        new JavaScriptObfuscator({
+            rotateStringArray: true
+        })
+    ],
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+    },
+    optimization: {
+        usedExports: false
+    }
 };
