f4283a66179032bc7af693c40b80826fdf6af711
Peter Walsh <peter.t.walsh@stonybrook.edu>
2022-10-30 11:13:15 -0400

    Library.

--- CRDTwrapper/src/index.ts
+++ CRDTwrapper/src/index.ts
@@ -1,5 +1,5 @@
 import * as Y from 'yjs';
-// var QuillDeltaToHtmlConverter = require('quill-delta-to-html').QuillDeltaToHtmlConverter;
+var QuillDeltaToHtmlConverter = require('quill-delta-to-html').QuillDeltaToHtmlConverter;
 
 class CRDTFormat {
     public bold?: Boolean = false;
@@ -19,7 +19,7 @@ exports.CRDT = class {
         this.id = -1;
 
         this.ydoc.on("update", (update: Uint8Array, origin: any) => {
-            this.cb(JSON.stringify({update: Array.from(update), clientId: origin}), origin);
+            this.cb(JSON.stringify({update: Array.from(update), clientId: this.id}), origin);
         })
 
         this.cb = cb;
@@ -37,34 +37,35 @@ exports.CRDT = class {
     insert(index: number, content: string, format: CRDTFormat) {
         this.ydoc.transact(() => {
             this.ytext.insert(index, content, format);
-        }, true)
+        }, false)
     }
 
     delete(index: number, length: number) {
         this.ydoc.transact(() => {
             this.ytext.delete(index, length);
-        }, true);
+        }, false);
     }
 
     toHTML() {
-        // let converter = new QuillDeltaToHtmlConverter(this.ytext.toDelta(), )
-        let html = '';
+        let converter = new QuillDeltaToHtmlConverter(this.ytext.toDelta(), {});
+        return converter.convert()
+        // let html = '';
 
-        for (let op of this.ytext.toDelta()) {
-            let text = op.insert;
-            let attr = op.attributes;
-            if (attr ? attr.underline : false) {
-                text = "<u>" + text + "</u>";
-            }
-            if (attr ? attr.italic : false) {
-                text = "<em>" + text + "</em>";
-            }
-            if (attr ? attr.bold : false) {
-                text = "<strong>" + text + "</strong>";
-            }
-            html += text;
-        }
-        return "<p>" + html + "</p>";
+        // for (let op of this.ytext.toDelta()) {
+        //     let text = op.insert;
+        //     let attr = op.attributes;
+        //     if (attr ? attr.underline : false) {
+        //         text = "<u>" + text + "</u>";
+        //     }
+        //     if (attr ? attr.italic : false) {
+        //         text = "<em>" + text + "</em>";
+        //     }
+        //     if (attr ? attr.bold : false) {
+        //         text = "<strong>" + text + "</strong>";
+        //     }
+        //     html += text;
+        // }
+        // return "<p>" + html + "</p>";
     }
 };
 
